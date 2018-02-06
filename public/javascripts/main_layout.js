a082f3590a955e682552a9f95592536e8691a89a
Lucien <dev.myhorsefamily@gmail.com>
2018-02-06 15:58:55 +0100

    modif couleur

--- public/javascripts/main_layout.js
+++ public/javascripts/main_layout.js
@@ -1,8 +1,10 @@
-const icon_control_btn = document.getElementById('icon_control_btn');
-document.getElementById('sidebar_btn').addEventListener("click", (e)=>{
-    $("#sidebar").animate({width:'toggle'}, 'fast');
-
-
-    icon_control_btn.innerText = icon_control_btn.innerText==='keyboard_arrow_left' ? "keyboard_arrow_right":"keyboard_arrow_left";
-
-});
+// const icon_control_btn = document.getElementById('icon_control_btn');
+// document.getElementById('sidebar_btn').addEventListener("click", (e)=>{
+//
+//     $("#sidebar").animate({width:'toggle'}, 'fast');
+//     const style = getComputedStyle(document.getElementById("sidebar"));
+//     document.getElementById('sidebar_btn').style.marginLeft = style.width.substring(0, style.width.length-2) < 2 ? "0px" : "20px";
+//
+//     icon_control_btn.innerText = icon_control_btn.innerText==='keyboard_arrow_left' ? "keyboard_arrow_right":"keyboard_arrow_left";
+//
+// });
