c03adfc36190553f2eb25e14aedbdcc2ada8316c
Lucien <dev.myhorsefamily@gmail.com>
2018-03-12 16:39:59 +0100

    vue-app w/ vuetify

---
+++ views/vue-app/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  margin-top: 60px;
+}
+</style>
